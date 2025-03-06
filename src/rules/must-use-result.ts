cf0847880f2d15f9ff894eb8ca0336cbc5ff7cda
SIHAN LI <1015813038@qq.com>
2025-03-06 06:48:58 -0500

    Fix: #10 (#17)
    * [Refactor]  wrap the assignment check using a helper for later refactor reason
    
    * [Fix] now we can await the promise properly
    
    * [Chore] mark the first assigned variable to be reported if possible
    
    * [Chore]
    
    * [Fix] typo
    
    * [Chore] write `AwaitExpression` inside `resultSelector`
    
    * [Test] unit test for #10

--- src/rules/must-use-result.ts
+++ src/rules/must-use-result.ts
@@ -14,6 +14,7 @@ const resultSelector = matchAny([
   // 'Identifier',
   'CallExpression',
   'NewExpression',
+  'AwaitExpression',
 ]);
 
 const resultProperties = [
@@ -130,12 +131,16 @@ const ignoreParents = [
   'ClassProperty',
 ];
 
+/**
+ * @returns A boolean indicates if the node is not handled.
+ */
 function processSelector(
   context: TSESLint.RuleContext<MessageIds, []>,
   checker: TypeChecker,
   parserServices: ParserServices,
   node: TSESTree.Node,
-  reportAs = node
+  reportAs = node,
+  isReferenceNode = false
 ): boolean {
   if (node.parent?.type.startsWith('TS')) {
     return false;
@@ -155,31 +160,25 @@ function processSelector(
     return false;
   }
 
-  const assignedTo = getAssignation(checker, parserServices, node);
-  const currentScope = context.getScope();
-
-  // Check if is assigned
-  if (assignedTo) {
-    const variable = currentScope.set.get(assignedTo.name);
-    const references =
-      variable?.references.filter((ref) => ref.identifier !== assignedTo) ?? [];
-    if (references.length > 0) {
-      return references.some((ref) =>
-        processSelector(
-          context,
-          checker,
-          parserServices,
-          ref.identifier,
-          reportAs
-        )
-      );
-    }
+  const anyHandled = handleAssignation(
+    context,
+    checker,
+    parserServices,
+    node,
+    reportAs
+  );
+  if (anyHandled) {
+    return false;
   }
 
-  context.report({
-    node: reportAs,
-    messageId: MessageIds.MUST_USE,
-  });
+  // make sure not reporting to the same node mutiple times during recursing calls
+  if (!isReferenceNode) {
+    context.report({
+      node: reportAs,
+      messageId: MessageIds.MUST_USE,
+    });
+  }
+
   return true;
 }
 
@@ -219,3 +218,42 @@ const rule: TSESLint.RuleModule<MessageIds, []> = {
 };
 
 export = rule;
+
+function handleAssignation(
+  context: TSESLint.RuleContext<MessageIds, []>,
+  checker: TypeChecker,
+  parserServices: ParserServices,
+  node: TSESTree.Node,
+  reportAs: TSESTree.Node = node
+): boolean {
+  const assignedTo = getAssignation(checker, parserServices, node);
+  const currentScope = context.getScope();
+
+  // Check if is assigned to variables
+  if (assignedTo) {
+    const variable = currentScope.set.get(assignedTo.name);
+    const references =
+      variable?.references.filter((ref) => ref.identifier !== assignedTo) ?? [];
+
+    /**
+     * Try to mark the first assigned variable to be reported, if not, keep
+     * the original one.
+     */
+    reportAs = variable?.references[0].identifier ?? reportAs;
+
+    // check if any reference is handled by recursive calling
+    return references.some(
+      (ref) =>
+        !processSelector(
+          context,
+          checker,
+          parserServices,
+          ref.identifier,
+          reportAs,
+          true
+        )
+    );
+  }
+
+  return false;
+}
